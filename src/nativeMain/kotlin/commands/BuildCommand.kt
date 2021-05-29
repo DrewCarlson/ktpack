ec61647bfcf810d6656f59f25e96ca52016f35de
Drew Carlson <awcarlson@protonmail.com>
2021-05-29 12:09:01 -0700

    Cleanup new command, multibin build

--- src/nativeMain/kotlin/commands/BuildCommand.kt
+++ src/nativeMain/kotlin/commands/BuildCommand.kt
@@ -1,10 +1,12 @@
 package ktpack.commands
 
 import com.github.ajalt.clikt.core.*
-import com.github.ajalt.mordant.terminal.*
+import com.github.ajalt.clikt.parameters.options.*
 import com.github.ajalt.mordant.rendering.TextColors.*
+import com.github.ajalt.mordant.terminal.*
 import ktpack.*
 import ktpack.subprocess.*
+import ktpack.util.*
 import me.archinamon.fileio.*
 import kotlin.math.*
 import kotlin.time.*
@@ -13,6 +15,22 @@ import kotlin.time.DurationUnit.*
 class BuildCommand(private val term: Terminal) : CliktCommand(
     help = "Compile packages and dependencies.",
 ) {
+
+    private val releaseMode by option("--release")
+        .help("Build artifacts in release mode, with optimizations")
+        .flag(default = false)
+
+    private val targetBin by option("--bin")
+        .help("Build only the specified binary")
+
+    private val libOnly by option("--lib")
+        .help("Build only this package's library")
+        .flag(default = false)
+
+    private val binsOnly by option("--bins")
+        .help("Build all binaries")
+        .flag(default = false)
+
     override fun run() {
         val manifest = loadManifest(MANIFEST_NAME)
         val module = manifest.module
@@ -20,39 +38,80 @@ class BuildCommand(private val term: Terminal) : CliktCommand(
         val srcFolder = File("src")
         check(srcFolder.isDirectory()) { "Expected `src` file to be a directory." }
 
-        if (module.autobin) {
-            val mainSource = srcFolder.listFiles().find { it.getName() == "main.kt" }
-            if (mainSource == null) {
-                //TODO("Only `src/main.kt` autobin is supported at this time.")
-            } else {
-                val outDir = File("out/")
-                if (!outDir.exists()) outDir.mkdirs()
+        val srcFiles = srcFolder.listFiles()
+        val mainSource = srcFiles.find { it.getName() == "main.kt" }
+        val binFolder = File("src/bin")
+        val otherBins = if (binFolder.exists()) binFolder.listFiles() else emptyArray()
+        val otherBinPaths = otherBins.map(File::getAbsolutePath)
 
-                val sourceFiles = srcFolder.walkTopDown().toList()
-                    .filter { it.getAbsolutePath() != mainSource.getAbsolutePath() }
+        val outDir = File("out/")
+        if (!outDir.exists()) outDir.mkdirs()
 
-                term.println("${brightGreen("Compiling")} ${module.name} v${module.version} (${srcFolder.getParent()})")
-                val compileDuration = measureTime {
-                    exec {
-                        // TODO: actual kotlinc lookup and default selection behavior kotlinc/kotlinc-native
-                        arg("/usr/local/bin/kotlinc-native")
-                        //arg("%homepath%/.konan/kotlin-native-prebuilt-windows-1.5.10/bin/kotlinc-native.bat")
-                        // TODO: actual search for main functions and output details
-                        arg(mainSource.getAbsolutePath())
-                        sourceFiles.forEach { file ->
-                            arg(file.getAbsolutePath())
-                        }
-                        arg("-o")
-                        arg("out/main")
-                    }
-                }.toDouble(SECONDS).roundTo(2)
-                term.println("${brightGreen("Finished")} dev [unoptimized + debuginfo] target(s) in ${compileDuration}s")
+        // Collect other non-bin source files
+        val sourceFiles = srcFolder
+            .walkTopDown()
+            .filter { file ->
+                val absolutePath = file.getAbsolutePath()
+                file.getName().endsWith(".kt") &&
+                    absolutePath != mainSource.getAbsolutePath() &&
+                    !otherBinPaths.contains(absolutePath)
+            }
+            .toList()
+
+        term.println("${success("Compiling")} ${module.name} v${module.version} (${srcFolder.getParent()})")
+        val compileDuration = measureSeconds {
+            val baseOutPath = "out"
+            if (mainSource?.exists() == true) {
+                val mainBinName = mainSource.nameWithoutExtension
+                val outputPath = "$baseOutPath/$mainBinName"
+                compileBin(mainSource, sourceFiles, releaseMode, outputPath)
+            }
+            otherBins.forEach { otherBin ->
+                val otherBinName = otherBin.nameWithoutExtension
+                val otherOutputPath = "$baseOutPath/$otherBinName"
+                compileBin(otherBin, sourceFiles, releaseMode, otherOutputPath)
             }
         }
+        term.println(buildString {
+            append(success("Finished"))
+            if (releaseMode) {
+                append(" release [optimized] target(s)")
+            } else {
+                append(" dev [unoptimized + debuginfo] target(s)")
+            }
+            append(" in ${compileDuration}s")
+        })
     }
 }
 
-fun Double.roundTo(numFractionDigits: Int): Double {
-    val factor = 10.0.pow(numFractionDigits.toDouble())
-    return (this * factor).roundToInt() / factor
+private fun compileBin(
+    mainSource: File,
+    sourceFiles: List<File>,
+    releaseMode: Boolean,
+    outputPath: String,
+) {
+    exec {
+        // TODO: actual kotlinc lookup and default selection behavior kotlinc/kotlinc-native
+        arg("/usr/local/bin/kotlinc-native")
+        //arg("%homepath%/.konan/kotlin-native-prebuilt-windows-1.5.10/bin/kotlinc-native.bat")
+
+        arg(mainSource.getAbsolutePath())
+        sourceFiles.forEach { file ->
+            arg(file.getAbsolutePath())
+        }
+
+        // kotlinc (jvm) only: output folder/ZIP/Jar path
+        //arg("-p")
+        //arg("path")
+
+        // kotlinc-native only: binary output path+name
+        arg("-o")
+        arg(outputPath)
+
+        if (releaseMode) {
+            arg("-opt") // kotlinc-native only: enable compilation optimizations
+        } else {
+            arg("-g") // kotlinc-native only: emit debug info
+        }
+    }
 }
