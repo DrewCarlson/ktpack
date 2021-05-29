4190e16e513a9de4e0a5e1fbb08be1907154b66e
Drew Carlson <awcarlson@protonmail.com>
2021-05-29 12:07:59 -0700

    Add util package

---
+++ src/nativeMain/kotlin/util/styles.kt
@@ -0,0 +1,7 @@
+package ktpack.util
+
+import com.github.ajalt.mordant.rendering.TextColors.*
+import com.github.ajalt.mordant.rendering.TextStyles.*
+
+val success = (bold + brightGreen)
+val failed = (bold + brightRed)
