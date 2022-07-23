c0ee891b71eac05d0db1d69802fbf73c0fd8e613
Drew Carlson <awcarlson@protonmail.com>
2022-07-22 18:04:29 -0700

    Fix tomlc99 arm assemble task creation

--- libs/tomlc99/build.gradle.kts
+++ libs/tomlc99/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     `cpp-library`
 }
 
+val hostOs = DefaultNativePlatform.getCurrentOperatingSystem()
+
 library {
     baseName.set("tomlc99")
     linkage.set(listOf(Linkage.STATIC))
@@ -26,7 +28,10 @@ library {
 
 fun Task.assembleMacosArm64(type: String) {
     val outPath = "build/lib/main/${type}/macos/arm64"
-    inputs.files(rootProject.file("external/tomlc99/toml.c"), rootProject.file("external/tomlc99/toml.h"))
+    inputs.files(
+        rootProject.file("external/tomlc99/toml.c"),
+        rootProject.file("external/tomlc99/toml.h"),
+    )
     outputs.files(file("$outPath/libtomlc99.a"))
     doFirst {
         val execResult = exec {
@@ -49,7 +54,7 @@ fun Task.assembleMacosArm64(type: String) {
     }
 }
 
-if (DefaultNativePlatform.getCurrentOperatingSystem().isMacOsX) {
-    tasks.create("assembleDebugMacosArm64") { assembleMacosArm64("debug") }
-    tasks.create("assembleReleaseMacosArm64") { assembleMacosArm64("release") }
+if (hostOs.isMacOsX) {
+    tasks.register("assembleDebugMacosArm64") { assembleMacosArm64("debug") }
+    tasks.register("assembleReleaseMacosArm64") { assembleMacosArm64("release") }
 }
