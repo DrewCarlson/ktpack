144cad8050352fdb7370b05df6f3a68a44a543da
Drew Carlson <awcarlson@protonmail.com>
2023-08-04 18:22:12 -0700

    Create ktpack dir with okio

--- ktpack/src/commonMain/kotlin/ktpack/commands/KtpackCommand.kt
+++ ktpack/src/commonMain/kotlin/ktpack/commands/KtpackCommand.kt
@@ -8,6 +8,7 @@ import com.github.ajalt.mordant.terminal.*
 import io.ktor.client.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.utils.io.core.*
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import ktfio.File
@@ -44,15 +45,13 @@ class KtpackCommand(
     override val config: KtpackUserConfig by lazy {
         File(KTPACK_ROOT, "config.json").run {
             if (!exists()) {
-                FileSystem.SYSTEM.createDirectory(KTPACK_ROOT.toPath(), mustCreate = true)
+                FileSystem.SYSTEM.createDirectory(KTPACK_ROOT.toPath(), mustCreate = false)
                 //check(File(KTPACK_ROOT).mkdirs()) {
                 //    "Failed to create Ktpack folder $KTPACK_ROOT"
                 //}
                 println(getAbsolutePath())
-                if (createNewFile()) {
-                    writeText(json.encodeToString(KtpackUserConfig()))
-                } else {
-                    error("Failed to write: ${getAbsolutePath()}")
+                FileSystem.SYSTEM.write("${KTPACK_ROOT}/config.json".toPath(true), true) {
+                    write(json.encodeToString(KtpackUserConfig()).toByteArray())
                 }
             }
 
