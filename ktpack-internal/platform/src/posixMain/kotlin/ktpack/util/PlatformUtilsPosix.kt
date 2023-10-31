127d661d29ac4468d6700e386fe5237156f17d31
Drew Carlson <awcarlson@protonmail.com>
2023-10-31 11:42:10 -0700

    Change posix working directory to Path

--- ktpack-internal/platform/src/posixMain/kotlin/ktpack/util/PlatformUtilsPosix.kt
+++ ktpack-internal/platform/src/posixMain/kotlin/ktpack/util/PlatformUtilsPosix.kt
@@ -2,15 +2,17 @@ package ktpack.util
 
 import kotlinx.cinterop.*
 import platform.posix.*
+import okio.Path
+import okio.Path.Companion.toPath
 
 actual fun getHomePath(): String? {
     return getenv("HOME")?.toKString()
         ?: getpwuid(getuid())?.pointed?.pw_dir?.toKString()
 }
 
-actual val workingDirectory: String by lazy {
+actual val workingDirectory: Path by lazy {
     memScoped {
         val bufLen = 250
-        allocArray<ByteVar>(bufLen).apply { getcwd(this, bufLen.convert()) }.toKString()
+        allocArray<ByteVar>(bufLen).apply { getcwd(this, bufLen.convert()) }.toKString().toPath()
     }
 }
