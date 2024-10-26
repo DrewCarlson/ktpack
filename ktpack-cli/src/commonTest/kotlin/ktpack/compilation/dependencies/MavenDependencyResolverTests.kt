47e3a4c82e14b25b39bc9919acfdf7a548538515
Drew Carlson <awcarlson@protonmail.com>
2024-10-26 13:40:57 -0700

    Update MavenDependencyResolverTests

--- ktpack-cli/src/commonTest/kotlin/ktpack/compilation/dependencies/MavenDependencyResolverTests.kt
+++ ktpack-cli/src/commonTest/kotlin/ktpack/compilation/dependencies/MavenDependencyResolverTests.kt
@@ -35,7 +35,7 @@ class MavenDependencyResolverTests {
             context,
             modulePath = sampleRoot,
         )
-        resolver = MavenDependencyResolver(module, context.http)
+        resolver = MavenDependencyResolver(context.http)
     }
 
     @Test
@@ -49,29 +49,30 @@ class MavenDependencyResolverTests {
             .map { it.substringAfterLast(DIRECTORY_SEPARATOR) }
 
         val expected = listOf(
-            "kotlinx-atomicfu-runtime-1.8.20.klib",
-            "atomicfu-jsir-0.20.2.klib",
-            "kotlinx-coroutines-core-jsir-1.7.1.klib",
-            "kotlinx-serialization-core-jsir-1.3.3.klib",
-            "kotlinx-serialization-json-jsir-1.3.3.klib",
-            "kotlin-dom-api-compat-1.9.22.klib",
-            "ktor-io-js-2.3.8.klib",
-            "ktor-utils-js-2.3.8.klib",
-            "ktor-http-js-2.3.8.klib",
-            "ktor-events-js-2.3.8.klib",
-            "ktor-websockets-js-2.3.8.klib",
-            "ktor-serialization-js-2.3.8.klib",
-            "ktor-websocket-serialization-js-2.3.8.klib",
-            "ktor-client-core-js-2.3.8.klib",
             "kermit-js-2.0.3.klib",
+            "kotlin-dom-api-compat-2.0.0.klib",
             "kermit-core-js-2.0.3.klib",
-            "atomicfu-1.6.21.jar",
-            "ktor-client-content-negotiation-jsir-2.0.3.klib",
-            "ktor-serialization-kotlinx-jsir-2.0.3.klib",
-            "ktor-serialization-kotlinx-json-jsir-2.0.3.klib",
-            "coingecko-js-1.0.0-beta02.klib",
-            "ktor-client-js-js-2.3.8.klib",
+            "coingecko-js-1.0.0-rc01.klib",
+            "kotlinx-atomicfu-runtime-1.9.21.klib",
+            "kotlinx-coroutines-core-js-1.8.1.klib",
+            "atomicfu-js-0.23.1.klib",
+            "kotlinx-serialization-json-js-1.6.0.klib",
+            "kotlinx-serialization-core-js-1.6.0.klib",
+            "ktor-client-core-js-2.3.11.klib",
+            "ktor-http-js-2.3.11.klib",
+            "ktor-utils-js-2.3.11.klib",
+            "ktor-io-js-2.3.11.klib",
+            "ktor-events-js-2.3.11.klib",
+            "ktor-websocket-serialization-js-2.3.11.klib",
+            "ktor-serialization-js-2.3.11.klib",
+            "ktor-websockets-js-2.3.11.klib",
+            "ktor-client-content-negotiation-js-2.3.6.klib",
+            "ktor-serialization-kotlinx-json-js-2.3.6.klib",
+            "ktor-serialization-kotlinx-js-2.3.6.klib",
+            "atomicfu-js-0.22.0.klib",
+            "ktor-client-js-js-2.3.11.klib",
         )
+        println(actual.joinToString("\n"))
         expected.forEach { assertTrue(actual.contains(it), "missing from actual: $it") }
         actual.forEach { assertTrue(expected.contains(it), "missing from expect: $it") }
     }
@@ -87,32 +88,35 @@ class MavenDependencyResolverTests {
             .map { it.substringAfterLast(DIRECTORY_SEPARATOR) }
 
         val expected = listOf(
-            "annotations-23.0.0.jar",
-            "kotlinx-coroutines-core-jvm-1.7.1.jar",
-            "kotlinx-serialization-core-jvm-1.3.3.jar",
-            "kotlinx-serialization-json-jvm-1.3.3.jar",
-            "kotlinx-coroutines-jdk8-1.7.1.jar",
-            "slf4j-api-1.7.36.jar",
-            "ktor-io-jvm-2.3.8.jar",
-            "ktor-utils-jvm-2.3.8.jar",
-            "ktor-http-jvm-2.3.8.jar",
-            "ktor-events-jvm-2.3.8.jar",
-            "ktor-websockets-jvm-2.3.8.jar",
-            "ktor-serialization-jvm-2.3.8.jar",
-            "ktor-websocket-serialization-jvm-2.3.8.jar",
-            "kotlinx-coroutines-slf4j-1.7.1.jar",
-            "ktor-client-core-jvm-2.3.8.jar",
-            "ktor-client-content-negotiation-jvm-2.0.3.jar",
-            "ktor-serialization-kotlinx-jvm-2.0.3.jar",
-            "ktor-serialization-kotlinx-json-jvm-2.0.3.jar",
-            "coingecko-jvm-1.0.0-beta02.jar",
             "kermit-jvm-2.0.3.jar",
             "kermit-core-jvm-2.0.3.jar",
-            "ktor-network-jvm-2.3.8.jar",
-            "ktor-http-cio-jvm-2.3.8.jar",
-            "ktor-network-tls-jvm-2.3.8.jar",
-            "ktor-client-cio-jvm-2.3.8.jar",
+            "coingecko-jvm-1.0.0-rc01.jar",
+            "kotlinx-coroutines-core-jvm-1.8.1.jar",
+            "annotations-23.0.0.jar",
+            "kotlinx-serialization-json-jvm-1.6.0.jar",
+            "kotlinx-serialization-core-jvm-1.6.0.jar",
+            "ktor-client-core-jvm-2.3.11.jar",
+            "kotlinx-coroutines-jdk8-1.7.1.jar",
+            "slf4j-api-1.7.36.jar",
+            "ktor-http-jvm-2.3.11.jar",
+            "ktor-utils-jvm-2.3.11.jar",
+            "ktor-io-jvm-2.3.11.jar",
+            "ktor-events-jvm-2.3.11.jar",
+            "ktor-websocket-serialization-jvm-2.3.11.jar",
+            "ktor-serialization-jvm-2.3.11.jar",
+            "ktor-websockets-jvm-2.3.11.jar",
+            "kotlinx-coroutines-slf4j-1.7.1.jar",
+            "ktor-client-content-negotiation-jvm-2.3.6.jar",
+            "ktor-serialization-kotlinx-json-jvm-2.3.6.jar",
+            "ktor-serialization-kotlinx-jvm-2.3.6.jar",
+            "atomicfu-jvm-0.22.0.jar",
+            "ktor-client-cio-jvm-2.3.11.jar",
+            "ktor-http-cio-jvm-2.3.11.jar",
+            "ktor-network-jvm-2.3.11.jar",
+            "ktor-network-tls-jvm-2.3.11.jar",
+            "slf4j-simple-1.7.36.jar",
         )
+        println(actual.joinToString("\n"))
         expected.forEach { assertTrue(actual.contains(it), "missing from actual: $it") }
         actual.forEach { assertTrue(expected.contains(it), "missing from expect: $it") }
     }
